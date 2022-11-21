13e808a5eb17eedb5b0a6eccec69176643f30087
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-11-21 11:54:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/c/GreyMattersBot') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('❣ 𝐉𝐎𝐈𝐍 𝐂𝐇𝐀𝐍𝐍𝐋 ❣', url='https://t.me/DK_Gru') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
