f0d85a1dde639bdbca1ca01499e1f91ccbc7e0a8
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-11-25 12:02:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Dkmovie/DK_Moviefilter-url-shortener-bot /Eva
+  git clone https://github.com/Dkmovie/DK_Movie_filterv2 /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
