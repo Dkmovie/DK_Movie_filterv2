42afb030083bc33de78535528b1953f9188dcad6
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-12-09 09:29:43 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'Hello, World!'
+
+#Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
+os.system("git clone https://github.com/Dkmovie/DK_Movie_filterv2 okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m telegram &")
